5afd94ae419e3ae9727a7489d15242d6ccf50a0b
Sudesh Subedi <nooneandnothing09@gmail.com>
2025-07-30 22:17:30 +0545

    Now, learnt about make_response(for custom headers, adding custom status code and also making custom POST, GET etc. requests as per need

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 
-def index():
-    return '<h1> Hello World </h1>'
+def hello():
+    response = make_response("HEY\n")
+    response.status_code = 202
+    response.headers['content-type'] = 'this is customized '
+    response.headers['date'] = 'we dont know yet'
+    return response
+
+
+# @app.route('/hello', methods = ['POST', 'GET'] )
+# def hey():
+#     if request.method == "GET":
+#         return "it's get request\n"
+    
+#     elif request.method == "POST":
+#         return "it's post  request\n"
+    
+#     else:
+#         return "it's get request"
+    
 
 @app.route('/greet/<name>')
 
@@ -13,7 +30,7 @@ def greet(name):
     return f"hello {name}"
 
 @app.route('/add/<number1>/<number2>')
-
+  
 def add(number1, number2):
     number1 = int(number1)
     number2 = int(number2)
@@ -23,11 +40,14 @@ def add(number1, number2):
 @app.route('/handle_url_params')
 
 def handle_url_params():
+    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
+        greeting = request.args.get('greeting')
+        name = request.args.get('name')
+        return f"{greeting}, {name}"
 
-    greeting = request.args.get('greeting')
-    name = request.args.get('name')
-    return f"{greeting}, {name}"
+    else:
+        return "Some Params are missing."
 
 
 if __name__ == "__main__": 
-    app.run(debug=True)
+    app.run(debug=True,)
