fae69aead89bdab343a529b589f421dc93b82f53
Sudesh Subedi <nooneandnothing09@gmail.com>
2025-07-30 21:11:58 +0545

    Same day: Learnt about hadling url params, still going more

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-    return "<h1>Hello World</h1>"
-
-
-@app.route('/hello')
-def hello():
-    return "Hello World"
-
+    return '<h1> Hello World </h1>'
 
 @app.route('/greet/<name>')
 
 def greet(name):
     return f"hello {name}"
 
-
 @app.route('/add/<number1>/<number2>')
 
 def add(number1, number2):
     number1 = int(number1)
     number2 = int(number2)
-    return f'{number1} + {number2} = {number1+number2}'
+    return f"{number1} + {number2} = {number1+number2}"    
+
+
+@app.route('/handle_url_params')
+def handle_url_params():
+    return str(request.args)
+
 
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', debug=True)
+    app.run(debug=True)
