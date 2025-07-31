382626707a1f8e1eded8e75a590c13ad295a8205
Sudesh Subedi <nooneandnothing09@gmail.com>
2025-07-31 17:27:20 +0545

    Added custom filters.

--- app_2.py
+++ app_2.py
@@ -5,9 +5,36 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/')
 
 def index():
-    return render_template('index.html')
+    # myvalue = 'NeuraNine'
+    # myresult = 10*2000 
+    mylist = [10,20,30,40]
+    return render_template('index.html', mylist=mylist)
+   
 
+
+@app.route('/other')
+def other():
+    some_text = 'this is known to be some text'
+
+    return render_template('other.html', some_text=some_text)
+
+@app.template_filter('reverse_string')
+
+def reverse_string(s):
+    return s[::-1]
+
+
+@app.template_filter('repeat')
+
+def repeat(s,times=2):
+    return s*times
+
+@app.template_filter('alternate_case')
+
+def alternate_case(s):
+    return ''.join([c.upper() if i % 2 == 0 else c.lower() for i,c in enumerate(s)])
 
 if __name__ == "__main__":
     app.run(debug=True)
 
+ 
